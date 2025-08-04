53ad55c8235e3324dc40d001f90d6f142cf0e6e0
LikhithaMuppa <likhithamuppagowni102113@gmail.com>
2025-08-04 18:43:40 +0530

    Add files via upload

---
+++ RemoveSpecialCharacheter.java
@@ -0,0 +1,8 @@
+
+public class RemoveSpecialCharacheter {
+public static void main(String[] args) {
+	String s = "@$%^&& Selenium";
+	s = s.replaceAll("[^a-zA-Z0-9]","");		
+	System.out.println(s);
+}
+}
